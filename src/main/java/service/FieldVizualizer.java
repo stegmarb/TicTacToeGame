1744bc21580778359e694ec9251758716e15bf83
Balazs Stegmar <stegmarb@gmail.com>
2017-07-06 12:09:44 +0200

    feat(TicTacToeGame): add field class and field vizualizer class

---
+++ src/main/java/service/FieldVizualizer.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.Field;
+
+public class FieldVizualizer {
+
+  private static Field field = new Field();
+  private static int[][] currentField = field.getField();
+
+  public static void vizualizeField() {
+    
+  }
+}
